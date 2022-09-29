c7df35da66534a0a13b158322ecddaed5431d9bf
Muhammad Ghifari <ghifari9567@gmail.com>
2022-09-30 05:55:02 +0700

    Bump Version v2.6.11_93

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.wagyufari.dzikirqu"
         minSdk = 23
         targetSdk = 30
-        versionCode = 92
-        versionName = "2.6.10"
+        versionCode = 93
+        versionName = "2.6.11"
 
         vectorDrawables.useSupportLibrary = true
 
